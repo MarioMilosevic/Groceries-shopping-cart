82819af023472846dac791c8b7671db674ce0e5b
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-02 15:58:48 +0100

    Start on js files

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const vegetables = document.querySelector('#vegetables')
+const fruits = document.querySelector('#fruits')
 
+vegetables.addEventListener('click',function(){
+    
+})
+
+
+fruits.addEventListener('click',function(e){
+    e.preventDefault()
+    console.log('radi');
+})
